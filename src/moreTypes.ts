f9d35af1a984b0da816323d03c1dc71704ce90c2
Nakul <nakulsinghsisodia07@gmail.com>
2026-05-28 19:18:45 +0530

    force type assertiom with object and json

--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -1,3 +1,12 @@
+// force type assertion
 let res:any = '42'
- // force type 
 let len:number = (res as string).length
+
+type book = {
+    name: string
+}
+
+let bookString = '{"name":"atomic habit"}'
+let bookObj = JSON.parse(bookString) as  book
+
+console.log(bookObj.name);
