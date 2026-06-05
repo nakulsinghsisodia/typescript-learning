41316a1fbc5616a5c049b812a0d8d13146284958
Nakul <nakulsinghsisodia07@gmail.com>
2026-06-05 19:27:22 +0530

    learned enum in ts

--- src/arrayTupleEnums.ts
+++ src/arrayTupleEnums.ts
@@ -39,3 +39,43 @@ userInfo = ["abc",100,true]
 const location : readonly [number,number] = [2.33,45.6]
 
 const chaiItems : [name:string,price:number] = ["masala",10]
+
+// yeh push kr skte h tuple h to unexpected behaviour h yeh
+let t : [string,number] = ['a',1]
+t.push('b')
+
+//enums
+
+enum cupSize {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
+
+const size = cupSize.SMALL
+
+enum Status {
+    PENDING = 100,
+    SERVERD, //101
+    CANCELLED // 102
+}
+
+enum chaiType {
+    MASALA = 'masala',
+    ADRAK = 'adrak'
+}
+
+function makeChai (type : chaiType){
+    console.log(type);
+}
+
+makeChai(chaiType.MASALA)
+
+const enum Sugar {
+    LOW = 1,
+    MEDIUM = 2,
+    HIGH = 3
+}
+
+let s = Sugar.MEDIUM
+
