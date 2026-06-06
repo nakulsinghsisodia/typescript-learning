1c9e17c857ea25617d0a5fc348706b5a8dcdf47b
Nakul <nakulsinghsisodia07@gmail.com>
2026-06-06 19:26:13 +0530

    learned public private and protected in ts

---
+++ src/OOPs.ts
@@ -0,0 +1,40 @@
+class Chai{
+    flavour: string
+    price: number
+
+    constructor (flavour: string, price:number){
+        this.flavour = flavour
+        this.price = price
+    }
+}
+
+const masalaChai = new Chai("masala",12)
+
+
+class Tea{
+    public flavour : string = 'Elaichi'
+    private secretIng = 'boil for 2min'
+
+    reveal(){
+        return this.secretIng
+    }
+}
+
+new Tea().reveal()
+
+class Shop{
+    protected shopName = "Chai corner"
+}
+
+class Branch extends Shop{
+    getName(){
+        return this.shopName
+    }
+}
+
+new Branch().getName()
+
+class Walet{
+    #walet = 120 //private in js
+    _balance = 30 //protected
+}
