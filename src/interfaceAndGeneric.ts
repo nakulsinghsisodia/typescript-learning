5d25f75c94b590c5fc1a978894a47a7cfe3e2d10
Nakul <nakulsinghsisodia07@gmail.com>
2026-06-09 20:56:17 +0530

    learned interface for function in obj

--- src/interfaceAndGeneric.ts
+++ src/interfaceAndGeneric.ts
@@ -7,3 +7,18 @@ interface DiscountCall {
 const apply50: DiscountCall = (p)=>{return p*0.5}
 
 // interface for function in obj
+
+interface TeaMachine {
+    start(p:string):void
+
+    stop(p:string):void
+}
+
+const machine: TeaMachine = {
+    start(p) {
+        
+    },
+    stop(p) {
+        
+    },
+}
