421c99dd43caac4ce9194e5ef9bc5416192852af
Nakul <nakulsinghsisodia07@gmail.com>
2026-06-12 20:04:17 +0530

    learned axios with ts

--- src/webReq.ts
+++ src/webReq.ts
@@ -1,1 +1,19 @@
 // axios
+
+import axios, {type AxiosResponse} from 'axios'
+
+interface ToDo {
+    userId: number
+    id: number
+    title : string
+    completed: boolean
+}
+
+const fetch = async () =>{
+    try{
+        const res : AxiosResponse<ToDo> = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        console.log(res.data);
+    }catch(err:any){
+        console.log(err);
+    }
+}
